a1a631a682c80066dc68cb8ea9759e8e54d746e1
Peter Keenan <keenan@posteo.net>
2021-01-02 14:26:28 +0000

    No clear progress on nav toggles

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,6 @@ const hamburger = document.getElementById('hamburger');
 const navItems = document.getElementById('nav-items');
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('show');
-    navItems.classList.toggle('show');
+  // hamburger.classList.toggle('show');
+  navItems.classList.toggle('show');
 });
