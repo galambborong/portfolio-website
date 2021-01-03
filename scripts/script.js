0b4ad0ae96f5fff200589c7fc1d0403f10ae1ecc
Peter Keenan <keenan@posteo.net>
2021-01-03 10:12:28 +0000

    Hamburger working, but nav links are not

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,5 @@ const hamburger = document.getElementById('hamburger');
 const navItems = document.getElementById('nav-items');
 
 hamburger.addEventListener('click', () => {
-  // hamburger.classList.toggle('show');
   navItems.classList.toggle('show');
 });
