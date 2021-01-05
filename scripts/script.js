25717574e38f7aaef50c703dda97c3fba2da17e2
Peter Keenan <keenan@posteo.net>
2021-01-05 22:24:58 +0000

    Adding early functionality to project page

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
 const hamburger = document.getElementById('hamburger');
 const navItems = document.getElementById('nav-items');
 
+const projectBtn = document.getElementById('projectx');
+const projectDetails = document.getElementById('projecty');
+
+
 hamburger.addEventListener('click', () => {
-  navItems.classList.toggle('show');
+  navItems.classList.toggle('show-nav');
 });
+
+projectBtn.addEventListener('click', () => {
+  projectDetails.classList.toggle('show-proj');
+})
