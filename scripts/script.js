14d36f825d082d00116156f09e1c5ce8f1a272a1
Peter Keenan <keenan@posteo.net>
2021-05-07 16:07:03 +0100

    Clean up script.js and add skills in about/index.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,6 @@
-const hamburger = document.getElementById("hamburger");
-const navItems = document.getElementById("nav-items");
+const hamburger = document.getElementById('hamburger');
+const navItems = document.getElementById('nav-items');
 
-const projectBtn = document.getElementById("projectx");
-const projectDetails = document.getElementById("projecty");
-
-hamburger.addEventListener("click", () => {
-  navItems.classList.toggle("show-nav");
+hamburger.addEventListener('click', () => {
+  navItems.classList.toggle('show-nav');
 });
-
-// projectBtn.addEventListener('click', () => {
-//   projectDetails.classList.toggle('show-proj');
-// })
