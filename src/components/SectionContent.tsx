6eb7bca6bd67728501664a3fc77995559d399b82
Peter Keenan <keenan@posteo.net>
2021-07-01 17:38:10 +0100

    Add initial SectionContent component tests and logic

---
+++ src/components/SectionContent.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface IProps {
+  sectionContent: IContent
+}
+
+interface IContent {
+  sectionHeader: string,
+  body: string
+}
+
+const SectionContent: React.FC<IProps> = ({sectionContent}: IProps) => {
+  const {sectionHeader, body} = sectionContent;
+  return (<section className="section" data-testid="content">
+    <h3 className="section__title">{sectionHeader}</h3>
+    <p className="section__blurb">{body}</p>
+  </section>)  
+}
+
+export default SectionContent;
--- src/components/__tests__/SectionContent.test.tsx
+++ src/components/__tests__/SectionContent.test.tsx
@@ -7,3 +7,17 @@ const sampleSectionContent = {
   sectionHeader: 'Hello world',
   body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum sapiente voluptatibus pariatur sed delectus! Numquam magni neque veniam. Ipsam minima sapiente aut officiis libero aliquid architecto corrupti qui vitae. Fuga ipsam, alias sequi nihil ipsa placeat nobis quae qui atque ut aut assumenda consectetur delectus officiis, iste, animi impedit voluptatum.'
 };
+
+test('SectionContent component renders', () => {
+  const {getByTestId} = render(<SectionContent sectionContent={sampleSectionContent} />);
+  const sectionContent = getByTestId('content');
+  expect(sectionContent).toBeTruthy();
+})
+
+test('SectionContent renders text passed through props', () => {
+  const {getByText} = render(<SectionContent sectionContent={sampleSectionContent}/>);
+  const sectionHeader = getByText(sampleSectionContent.sectionHeader);
+  const sectionBody = getByText(sampleSectionContent.body);
+  expect(sectionHeader).toHaveTextContent(/Hello world/);
+  expect(sectionBody).toHaveTextContent(/Lorem ipsum dolor sit amet consectetur/);
+})
