0ed049d8595def64181c4d9f516a311114801330
Peter Keenan <keenan@posteo.net>
2021-07-01 21:29:50 +0100

    Add general GH repo api call

---
+++ src/utils/githubApi.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const getRepoInfo = async (repoSlug: string) => {
+    const { data } = await axios.get(`https://api.github.com/repos/galambborong/${repoSlug}`);
+    return data;
+}
