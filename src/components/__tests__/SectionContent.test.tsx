d22c9a1e2c108ee9521ac4fbc8bd1efdcbfcd337
Peter Keenan <keenan@posteo.net>
2021-07-01 17:04:47 +0100

    Add sample content to SectionContent

---
+++ src/components/__tests__/SectionContent.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import SectionContent from '../SectionContent';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+const sampleSectionContent = {
+  sectionHeader: 'Hello world',
+  body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum sapiente voluptatibus pariatur sed delectus! Numquam magni neque veniam. Ipsam minima sapiente aut officiis libero aliquid architecto corrupti qui vitae. Fuga ipsam, alias sequi nihil ipsa placeat nobis quae qui atque ut aut assumenda consectetur delectus officiis, iste, animi impedit voluptatum.'
+};
