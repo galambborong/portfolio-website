044a7060757a0ef407a72eb8edf69028c14bb37b
Peter Keenan <keenan@posteo.net>
2021-07-02 09:09:05 +0100

    Enforce style on keyFormatter function and tests

--- src/utils/keyFormatter.ts
+++ src/utils/keyFormatter.ts
@@ -1,4 +1,4 @@
 export const keyFormatter = (index: number, name: string) => {
-    if (name === undefined) return ""
-    return `${index}${name.slice(0, 3).toLowerCase()}`
-}
+  if (name === undefined) return '';
+  return `${index}${name.slice(0, 3).toLowerCase()}`;
+};
