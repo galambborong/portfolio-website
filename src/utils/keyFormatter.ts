ee8f230f5eda1577859cc20c95865bf768fa21ba
Peter Keenan <keenan@posteo.net>
2021-07-14 08:40:42 +0100

    Add return type to keyFormatter

--- src/utils/keyFormatter.ts
+++ src/utils/keyFormatter.ts
@@ -1,4 +1,4 @@
-export const keyFormatter = (index: number, name: string) => {
+export const keyFormatter = (index: number, name: string): string => {
   if (name === undefined) return '';
   return `${index}${name.slice(0, 3).toLowerCase()}`;
 };
