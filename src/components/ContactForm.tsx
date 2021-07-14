bf9a87cde2dd4aef146e8dd722d9e6ddc5e75890
Peter Keenan <keenan@posteo.net>
2021-07-14 08:41:17 +0100

    Add final basic scaffold parts for ContactForm component

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -5,6 +5,11 @@ const ContactForm: React.FC = () => {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Inside handleSubmit');
+  };
+
   const handleChange = (e: {
     target: {
       value: React.SetStateAction<string>;
@@ -24,20 +29,18 @@ const ContactForm: React.FC = () => {
   };
 
   return (
-    <form
-      // onSubmit={handleSubmit}
-      className="contact"
-      id="contact-form"
-    >
+    <form onSubmit={handleSubmit} className="contact" id="contact-form">
       <label htmlFor="name">
         <input
           type="text"
           id="name"
           value={name}
+          className="contact-input invalid"
           onChange={handleChange}
           placeholder="Enter your name"
         />
       </label>
+      <p id="name-invalid"></p>
       <label htmlFor="_replyto">
         <input
           type="text"
@@ -45,17 +48,20 @@ const ContactForm: React.FC = () => {
           value={email}
           onChange={handleChange}
           placeholder="Enter your email address"
+          className="contact-input invalid"
         />
       </label>
+      <p id="email-invalid"></p>
       <textarea
+        className="contact-input invalid"
         name="message"
         id="message"
-        cols="30"
-        rows="10"
         placeholder="Type your message here"
         value={message}
         onChange={handleChange}
       />
+      <p id="message-invalid"></p>
+      <button type="submit">Submit</button>
     </form>
   );
 };
